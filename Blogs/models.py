b677d0c4b59008d28d7f6a2d2e8cdc3022d72b27
sachinmukherjee <sachinmukherjee29@gmail.com>
2017-06-17 11:29:10 +0530

    Database added

--- Blogs/models.py
+++ Blogs/models.py
@@ -2,5 +2,45 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Login(models.Model):
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=20)
+
+
+class Users(models.Model):
+    fullname = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    state = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+    login_id = models.ForeignKey(Login)
+
+    def __str__(self):
+        return "%s %s %s %s %s" % (self.fullname, self.description, self.state, self.city, self.country)
+
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=40,primary_key=True)
+    content = models.TextField()
+    date = models.DateField("Date", default=date.today)
+    user_name = models.ForeignKey(Users)
+
+    def __str__(self):
+        return '%s %s %s %s' % (self.title, self.content, self.date, self.user_name)
+
+
+class Saved(models.Model):
+    title = models.ForeignKey(Blogs)
+
+    def __str__(self):
+        return self.title
+
+
+class ReadLater(models.Model):
+    title = models.ForeignKey(Blogs)
+
+    def __str__(self):
+        return self.title
